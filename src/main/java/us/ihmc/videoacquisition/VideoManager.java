667a318d8f0daa6754d81bbd410989470b0433eb
Sylvain Bertrand <sbertrand@ihmc.us>
2019-09-23 15:55:29 -0500

    Some cleanup and switch to scheduled executor to control publish Hz.

--- src/main/java/us/ihmc/videoacquisition/VideoManager.java
+++ src/main/java/us/ihmc/videoacquisition/VideoManager.java
@@ -1,20 +1,20 @@
 package us.ihmc.videoacquisition;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
 
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.Frame;
@@ -27,6 +27,7 @@ import us.ihmc.codecs.generated.YUVPicture;
 import us.ihmc.codecs.generated.YUVPicture.YUVSubsamplingType;
 import us.ihmc.codecs.yuv.JPEGEncoder;
 import us.ihmc.codecs.yuv.YUVPictureConverter;
+import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
 import us.ihmc.ros2.RealtimeRos2Node;
 import us.ihmc.ros2.RealtimeRos2Publisher;
@@ -35,105 +36,101 @@ import us.ihmc.util.PeriodicThreadSchedulerFactory;
 
 public class VideoManager
 {
-   private JTextField txtFieldDestinationAddr;
-   private JTextField txtFieldDestinationPort;
    private CanvasFrame mainFrame;
    private BufferedImage img;
 
+   private boolean started = false;
    private boolean showPreview = false;
    private boolean first = true;
    private boolean streamVideo = false;
 
    private PeriodicThreadSchedulerFactory threadFactory = new PeriodicNonRealtimeThreadSchedulerFactory();
-   private String name = "video-publisher";
+   private String name = "video_publisher";
    private String namespace = "/us/ihmc";
    private int domainId = -1; // FIXME set me up
    private RealtimeRos2Node ros2Node;
    private RealtimeRos2Publisher<VideoPacket> videoPacketPublisher;
    private Java2DFrameConverter frameConverter = new Java2DFrameConverter();
+   private final OpenCVFrameGrabber grabber;
 
    public VideoManager() throws IOException
    {
+      grabber = new OpenCVFrameGrabber(0);
+      grabber.setImageWidth(640);
+      grabber.setImageHeight(480);
+
       ros2Node = new RealtimeRos2Node(PubSubImplementation.FAST_RTPS, threadFactory, name, namespace, domainId);
       videoPacketPublisher = ros2Node.createPublisher(VideoPacket.getPubSubType().get(), "logging_camera_video_stream");
       ros2Node.spin();
+      ScheduledExecutorService executor = ThreadTools.newSingleDaemonThreadScheduledExecutor("video-grabber");
 
       try
       {
          img = ImageIO.read(new File("noVideo.png"));
       }
-      catch (IOException e1)
+      catch (IOException e)
       {
-         e1.printStackTrace();
+         e.printStackTrace();
       }
       setupUI();
 
-      Thread thread = new Thread()
+      executor.scheduleAtFixedRate(() ->
       {
-         @Override
-         public void run()
+         if (!started)
+            return;
+
+         try
          {
-            OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
-            grabber.setImageWidth(640);
-            grabber.setImageHeight(480);
+            Frame capturedFrame = grabber.grab();
 
-            try
+            if (capturedFrame != null)
             {
-               grabber.start();
-
-               Frame capturedFrame = null;
-
-               while ((capturedFrame = grabber.grab()) != null)
+               if (mainFrame.isVisible())
                {
-                  if (mainFrame.isVisible())
+                  // Show our frame in the preview
+
+                  if (showPreview)
                   {
-                     // Show our frame in the preview
-
-                     if (showPreview)
+                     SwingUtilities.invokeLater(() -> mainFrame.showImage(capturedFrame));
+                     first = true;
+                  }
+                  else
+                  {
+                     if (first)
                      {
-                        mainFrame.showImage(capturedFrame);
-                        first = true;
-                     }
-                     else
-                     {
-                        if (first)
-                        {
-                           first = false;
-                           mainFrame.showImage(img);
-                        }
-                     }
-
-                     if (streamVideo)
-                     {
-                        BufferedImage image = frameConverter.convert(capturedFrame);
-                        VideoPacket videoPacket = toVideoPacket(image);
-                        videoPacketPublisher.publish(videoPacket);
+                        first = false;
+                        SwingUtilities.invokeLater(() -> mainFrame.showImage(img));
                      }
                   }
-               }
 
-               mainFrame.dispose();
-
-            }
-            catch (Exception e)
-            {
-               e.printStackTrace();
-            }
-            finally
-            {
-               try
-               {
-                  grabber.close();
-               }
-               catch (Exception e)
-               {
-                  e.printStackTrace();
+                  if (streamVideo)
+                  {
+                     BufferedImage image = frameConverter.convert(capturedFrame);
+                     VideoPacket videoPacket = toVideoPacket(image);
+                     videoPacketPublisher.publish(videoPacket);
+                  }
                }
             }
          }
-      };
+         catch (Exception e)
+         {
+            e.printStackTrace();
+         }
+      }, 0, 100, TimeUnit.MILLISECONDS);
 
-      thread.start();
+      executor.execute(() ->
+      {
+         try
+         {
+            grabber.start();
+            started = true;
+         }
+         catch (Exception e)
+         {
+            e.printStackTrace();
+         }
+      });
+
    }
 
    private final YUVPictureConverter converter = new YUVPictureConverter();
@@ -178,36 +175,7 @@ public class VideoManager
       JCheckBox chckbxShowInputImage = new JCheckBox("Show Input Image");
       panel.add(chckbxShowInputImage);
 
-      chckbxShowInputImage.addActionListener(new ActionListener()
-      {
-
-         @Override
-         public void actionPerformed(ActionEvent e)
-         {
-            if (chckbxShowInputImage.isSelected())
-            {
-               showPreview = true;
-            }
-            else
-            {
-               showPreview = false;
-            }
-         }
-      });
-
-      JLabel lblDestinationAddress = new JLabel("Destination Address: ");
-      panel.add(lblDestinationAddress);
-
-      txtFieldDestinationAddr = new JTextField();
-      panel.add(txtFieldDestinationAddr);
-      txtFieldDestinationAddr.setColumns(10);
-
-      JLabel lblDestinationPort = new JLabel("Destination Port: ");
-      panel.add(lblDestinationPort);
-
-      txtFieldDestinationPort = new JTextField();
-      panel.add(txtFieldDestinationPort);
-      txtFieldDestinationPort.setColumns(10);
+      chckbxShowInputImage.addActionListener(e -> showPreview = chckbxShowInputImage.isSelected());
 
       JPanel panel_1 = new JPanel();
       mainFrame.getContentPane().add(panel_1, BorderLayout.SOUTH);
